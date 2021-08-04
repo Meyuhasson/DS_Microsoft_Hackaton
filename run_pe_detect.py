daa08869c092c68ffa7cefc2d35b5f2607b4f3fb
Raztv <70471696+Raztv@users.noreply.github.com>
2021-08-04 11:31:12 +0300

    Update run_pe_detect.py
    deleted comments

--- run_pe_detect.py
+++ run_pe_detect.py
@@ -1,3 +1,5 @@
+# Raz
+
 import os
 import argparse
 import pickle
@@ -9,23 +11,16 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import subprocess
 
-#FLOSS_PATH = r"C:\Users\leguy\OneDrive\Desktop\Raz\floss.exe"
-#files_path = r"C:\Users\leguy\OneDrive\Desktop\Raz\Second"
-#TEMP_PATH = r"C:\Windows\Temp"
-#DOWNLOAD_PATH = r"C:\Users\leguy\Downloads"
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", help="path to your pe file")
 args = parser.parse_args()
 path_to_pe = args.path
 
-#result = subprocess.run([FLOSS_PATH,path_to_pe], stdout=subprocess.PIPE)
 result = subprocess.run(["floss.exe",path_to_pe], stdout=subprocess.PIPE)
 file_strings = result.stdout.decode('ascii').replace("\r","")
 
-
-model_path = 'rfc_model.sav' #files_path + '\\rfc_model.sav'
-vectorizer_path = 'vectorizer.sav' #files_path + '\\vectorizer.sav'
+model_path = 'rfc_model.sav' 
+vectorizer_path = 'vectorizer.sav'
 
 loaded_model = pickle.load(open(model_path, 'rb'))
 vectorizer = pickle.load(open(vectorizer_path, 'rb'))
@@ -35,6 +30,5 @@ confidence = loaded_model.predict_proba(X)[0][result]
 most_important_vals = vectorizer.get_feature_names()[X.argmax()]
 dict_val = {"Malicious": str(result),"Event": "most suspicious str: " + most_important_vals,"Confidence": confidence,"Multicase": "Suspicious file strings"}
 
-print(dict_val)
 with open('output_file.json', 'w+') as fp:
     json.dump(dict_val, fp)
